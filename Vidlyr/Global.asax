15aea31427b5f6a4ba06757b3b68fd3da169fd83
Sathish Kumar <sathis810@gmail.com>
2017-03-01 06:56:56 +0530

    Add project files.

---
+++ Vidlyr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidlyr.MvcApplication" Language="C#" %>
---
+++ Vidlyr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidlyr.Startup))]
+namespace Vidlyr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
